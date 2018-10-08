942c8584c908d750705e68dfca03a7cdf40826b3
Marshall Bowers <mbowers@gravic.com>
2018-10-08 17:56:40 -0400

    Remove CLI

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
 import { extractIntl } from './lib'
 
-const program = require('commander')
-
-program
-  .version('0.0.1')
-  .usage('[options] <files>')
-  .parse(process.argv)
-
-const files = program.args
-
-extractIntl(files)
-  .then(messages => {
-    console.log(messages)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+export default extractIntl
